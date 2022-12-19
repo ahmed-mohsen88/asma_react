9861a38f82678ad24df8f38fd1610d7572e895e0
ahmed mohsen <ahmed.mohsen.web@gmail.com>
2022-12-19 09:22:53 +0200

    1st

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Login() {
+  return (
+    <Container>
+      <Content>content</Content>
+    </Container>
+  );
+}
+
+const Container = styled.section`
+  overflow: hidden;
+  display: flex;
+`;
+const Content = styled.div``;
+
+export default Login;
